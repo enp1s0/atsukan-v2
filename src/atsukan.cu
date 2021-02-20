aa7fda4323cdf4074ab8fc641e4cebc7d358a725
mutsuki <mutsuki@momo86.net>
2021-02-20 13:58:40 +0900

    Add grid search

--- src/atsukan.cu
+++ src/atsukan.cu
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <functional>
+#include <chrono>
+#include <thread>
 #include <cutf/memory.hpp>
 #include <cutf/nvrtc.hpp>
 #include <cuda_kernel_fusing.hpp>
 
 std::string gen_fma_kernel(const unsigned n_fma, const unsigned n_inner_loop) {
 
-	const auto loop_sentence = "for (unsigned i = 0; i < " + std::to_string(n_inner_loop) + "; i++) {";
+	const auto loop_sentence = "for (unsigned i = 0; i < " + std::to_string(n_inner_loop) + R"(; i++) { float a = src_ptr[(tid + i) % array_size];)";
 
 	cuda_kernel_fusing::kernel_constructor kernel_constructor(
-			"float* const dst_ptr, const float* const src_ptr",
+			"float* const dst_ptr, const float* const src_ptr, size_t array_size",
 			"const unsigned tid, float& a",
-			"const unsigned tid = threadIdx.x + blockIdx.x * blockDim.x; float a = src_ptr[tid];" + loop_sentence,
+			"const unsigned tid = threadIdx.x + blockIdx.x * blockDim.x;" + loop_sentence,
 			"dst_ptr[tid] = a;}"
 			);
 	kernel_constructor.add_device_function(
@@ -44,7 +46,7 @@ void run_kernel(const unsigned n_op, const unsigned n_inner_loop, const std::siz
 			NULL,
 			NULL);
 	const char *options[] = {
-		"--gpu-architecture=compute_75",
+		"--gpu-architecture=compute_86",
 	};
 	nvrtcResult result = nvrtcCompileProgram(program, 1, options);
 	size_t log_size;
@@ -82,22 +84,38 @@ void run_kernel(const unsigned n_op, const unsigned n_inner_loop, const std::siz
 
 	auto src_ptr = src_mem.get();
 	auto dst_ptr = dst_mem.get();
+	auto tmp_array_size = array_size;
 
 	const auto block_size = 256;
 	const auto grid_size = array_size / block_size;
 
+	const auto start_clock = std::chrono::high_resolution_clock::now();
 	for (std::size_t i = 0; i < n_execution; i++) {
 		cutf::nvrtc::launch_function(
 				cuFunction,
-				{&src_ptr, &dst_ptr},
+				{reinterpret_cast<void*>(&src_ptr), reinterpret_cast<void*>(&dst_ptr), reinterpret_cast<void*>(&tmp_array_size)},
 				grid_size,
 				block_size
 				);
 	}
 
 	CUTF_CHECK_ERROR(cudaDeviceSynchronize());
+	const auto end_clock = std::chrono::high_resolution_clock::now();
+	const auto elapsed_time_per_kernel = std::chrono::duration_cast<std::chrono::microseconds>(end_clock - start_clock).count() * 1e-6 / n_execution;
+	std::printf("[INFO] %20s : %e[s]", "time per kernel", elapsed_time_per_kernel);
 }
 
 int main() {
-	run_kernel(4, 16, 1lu << 30, gen_fma_kernel, 1lu << 15);
+	for (unsigned n_op_log = 1; n_op_log < 5; n_op_log++) {
+		for (unsigned n_inner_loop_log = 1; n_inner_loop_log < 5; n_inner_loop_log++) {
+			const unsigned n_op = 1u << n_op_log;
+			const unsigned n_inner_loop = 1u << n_inner_loop_log;
+
+			const std::size_t n_all_op = 1lu << 17;
+			const auto n_execution = n_all_op / (n_op * n_inner_loop);
+			run_kernel(n_op, n_inner_loop, 1lu << 30, gen_fma_kernel, n_execution);
+
+			std::this_thread::sleep_for(std::chrono::seconds(4));
+		}
+	}
 }
